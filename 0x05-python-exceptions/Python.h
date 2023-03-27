bb15f0b6640be0cfcce6bd82e9b45948bd5a113c
bekiassefa <bereketassefaalx@gmail.com>
2023-03-27 06:55:58 -0700

    task 10

---
+++ 0x05-python-exceptions/Python.h
@@ -0,0 +1,15 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+#endif /* LISTS_H */
