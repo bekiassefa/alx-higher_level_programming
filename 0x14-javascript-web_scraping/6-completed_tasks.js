634d87efd21d224640c4513dd737fd4a0c982fef
bekiassefa <bereketassefaalx@gmail.com>
2023-07-25 23:15:04 -0700

    task 6 update

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,20 +3,23 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, (error, response, body) => {
+request(url, function (error, response, body) {
   if (error) {
     console.log(error);
-  }
-  const todoDict = JSON.parse(body);
-  const compDict = {};
-  for (const task of todoDict) {
-    if (task.completed) {
-      if (compDict[task.userId]) {
-        compDict[task.userId] += 1;
-      } else {
-        compDict[task.userId] = 1;
+  } else {
+    const todos = JSON.parse(body);
+    const tasksCompleted = {};
+
+    for (const task of todos) {
+      if (task.completed) {
+        if (tasksCompleted[task.userId]) {
+          tasksCompleted[task.userId] += 1;
+        } else {
+          tasksCompleted[task.userId] = 1;
+        }
       }
     }
+
+    console.log(tasksCompleted);
   }
-  console.log(compDict);
 });
